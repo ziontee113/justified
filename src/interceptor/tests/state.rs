95e8c75fe1cf82cb0c7c9cfc22105a40498a524c
ziontee113 <ziontee113@gmail.com>
2022-12-01 19:07:06 +0700

    Update remove_fragment() test for State

--- src/interceptor/tests/state.rs
+++ src/interceptor/tests/state.rs
@@ -35,13 +35,27 @@ mod state_test {
 
         let l1_32_down = IncomingFragment::new("L1", 32, 1, mipoch(0));
         let l1_33_down = IncomingFragment::new("L1", 33, 1, mipoch(10));
+        let l1_34_down = IncomingFragment::new("L1", 34, 1, mipoch(15));
         state.add_fragment(l1_32_down);
         state.add_fragment(l1_33_down);
+        state.add_fragment(l1_34_down);
 
         let l1_32_up = IncomingFragment::new("L1", 32, 0, mipoch(40));
         state.remove_fragment(&l1_32_up);
 
-        assert_eq!(state.fragments().len(), 1);
+        assert_eq!(state.fragments().len(), 2);
         fragment_has_contents(state.fragments().get(0).unwrap(), "L1", 33, 1, mipoch(10));
+        fragment_has_contents(state.fragments().get(1).unwrap(), "L1", 34, 1, mipoch(15));
+
+        let l1_33_up = IncomingFragment::new("L1", 33, 0, mipoch(50));
+        state.remove_fragment(&l1_33_up);
+
+        assert_eq!(state.fragments().len(), 1);
+        fragment_has_contents(state.fragments().get(0).unwrap(), "L1", 34, 1, mipoch(15));
+
+        let l1_34_up = IncomingFragment::new("L1", 34, 0, mipoch(60));
+        state.remove_fragment(&l1_34_up);
+
+        assert_eq!(state.fragments().len(), 0);
     }
 }
