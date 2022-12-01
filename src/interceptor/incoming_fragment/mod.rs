31cee7d4a0f76c0a38a6c9340262d227673256dd
ziontee113 <ziontee113@gmail.com>
2022-12-01 21:52:20 +0700

    Separate test file for incoming_fragment module

--- src/interceptor/incoming_fragment/mod.rs
+++ src/interceptor/incoming_fragment/mod.rs
@@ -1,3 +1,5 @@
+mod test;
+
 use std::{fmt::Display, time::SystemTime};
 
 #[derive(Debug, Clone)]
@@ -63,29 +65,5 @@ impl IncomingFragment {
 impl Display for IncomingFragment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}|{}", self.key.device_alias, self.key.code)
-    }
-}
-
-#[cfg(test)]
-mod fragment_test {
-    use super::*;
-    use crate::utils::mipoch;
-
-    #[test]
-    fn can_make_new_fragment() {
-        let fragment = IncomingFragment::new("L1", 32, 1, mipoch(0));
-        assert_eq!(fragment.key.device_alias, "L1");
-        assert_eq!(fragment.key.code, 32);
-        assert_eq!(fragment.value(), 1);
-        assert_eq!(fragment.timestamp(), mipoch(0));
-    }
-
-    #[test]
-    fn test_display_trait_implementation() {
-        let fragment = IncomingFragment::new("L1", 32, 1, mipoch(0));
-        assert_eq!(fragment.to_string(), "L1|32");
-
-        let fragment = IncomingFragment::new("R2", 44, 0, mipoch(12));
-        assert_eq!(fragment.to_string(), "R2|44");
     }
 }
---
+++ src/interceptor/incoming_fragment/test.rs
@@ -0,0 +1,30 @@
+#[cfg(test)]
+mod incoming_fragment_test {
+    use crate::{interceptor::incoming_fragment::IncomingFragment, utils::mipoch};
+
+    #[test]
+    fn can_make_new_fragment() {
+        let fragment = IncomingFragment::new("L1", 32, 1, mipoch(0));
+        assert_eq!(fragment.key.device_alias, "L1");
+        assert_eq!(fragment.key.code, 32);
+        assert_eq!(fragment.value(), 1);
+        assert_eq!(fragment.timestamp(), mipoch(0));
+    }
+
+    #[test]
+    fn can_turn_fragment_to_string() {
+        let fragment = IncomingFragment::new("L1", 32, 1, mipoch(0));
+        assert_eq!(fragment.to_string(), "L1|32");
+
+        let fragment = IncomingFragment::new("R2", 44, 0, mipoch(12));
+        assert_eq!(fragment.to_string(), "R2|44");
+    }
+
+    #[test]
+    fn can_check_if_another_fragment_has_same_key_as_this_one() {
+        let before = IncomingFragment::new("L1", 32, 1, mipoch(0));
+        let after = IncomingFragment::new("L1", 32, 0, mipoch(49));
+
+        assert!(before.has_same_key(&after));
+    }
+}
