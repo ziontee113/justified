a4153c925c929f83da37efcd7f42656d9134547b
ziontee113 <ziontee113@gmail.com>
2022-12-02 15:36:10 +0700

    feat: add name field to RuleSet struct

--- src/rule_library/ruleset/mod.rs
+++ src/rule_library/ruleset/mod.rs
@@ -9,16 +9,20 @@ mod test;
 
 pub struct RuleSet {
     rules: HashMap<Vec<KeyIdentifier>, u16>,
+    name: String,
 }
 
 impl RuleSet {
-    pub fn new(rules: Vec<Rule>) -> Self {
+    pub fn new(name: &str, rules: Vec<Rule>) -> Self {
         let mut map = HashMap::new();
 
         for rule in rules {
             map.insert(rule.input().into(), rule.output());
         }
 
-        Self { rules: map }
+        Self {
+            name: name.to_string(),
+            rules: map,
+        }
     }
 }
--- src/rule_library/ruleset/test.rs
+++ src/rule_library/ruleset/test.rs
@@ -4,11 +4,14 @@ use super::RuleSet;
 
 #[test]
 fn can_create_new_ruleset_from_rules() {
-    let ruleset = RuleSet::new(vec![
-        rule!(L1 58 => 1),
-        rule!(L1 29, R1 36 => 116),
-        rule!(L1 29, R1 37 => 115),
-    ]);
+    let ruleset = RuleSet::new(
+        "Base Ruleset",
+        vec![
+            rule!(L1 58 => 1),
+            rule!(L1 29, R1 36 => 116),
+            rule!(L1 29, R1 37 => 115),
+        ],
+    );
 
     let first_key = rulekey!(L1 58);
     assert_eq!(*ruleset.rules.get(&first_key).unwrap(), 1);
