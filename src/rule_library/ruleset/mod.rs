707e2e9015729dda83d7dbf389be4e5ffca4d1e1
ziontee113 <ziontee113@gmail.com>
2022-12-02 22:38:53 +0700

    feat(ruleset): generate prefixes HashSet on create

--- src/rule_library/ruleset/mod.rs
+++ src/rule_library/ruleset/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::units::KeyIdentifier;
 
@@ -10,24 +10,32 @@ mod test;
 pub struct RuleSet {
     rules: HashMap<Vec<KeyIdentifier>, u16>,
     name: String,
+    prefixes: HashSet<Vec<KeyIdentifier>>,
 }
 
 impl RuleSet {
     pub fn new(name: &str, rules: Vec<Rule>) -> Self {
         let mut map = HashMap::new();
+        let mut prefixes = HashSet::new();
 
         for rule in rules {
             map.insert(rule.input().into(), rule.output());
+            prefixes.insert(generate_prefix_from_input(rule.input()));
         }
 
         Self {
             name: name.to_string(),
             rules: map,
+            prefixes,
         }
     }
 
     pub fn rules(&self) -> &HashMap<Vec<KeyIdentifier>, u16> {
         &self.rules
+    }
+
+    pub fn prefixes(&self) -> &HashSet<Vec<KeyIdentifier>> {
+        &self.prefixes
     }
 }
 
--- src/rule_library/ruleset/test.rs
+++ src/rule_library/ruleset/test.rs
@@ -41,3 +41,21 @@ fn can_generate_key_prefix_from_rule_input() {
     assert!(prefix.get(0).unwrap().is("L1", 58));
     assert!(prefix.get(1).unwrap().is("R1", 44));
 }
+
+#[test]
+fn can_generate_prefix_hash_set_when_creating_ruleset() {
+    let ruleset = RuleSet::new(
+        "Base Ruleset",
+        vec![
+            rule!(L1 58 => 1),
+            rule!(L1 29, R1 36 => 116),
+            rule!(L1 29, R1 36, R1 37 => 1),
+            rule!(L1 29, R1 37 => 115),
+        ],
+    );
+    let prefixes = ruleset.prefixes();
+    assert_eq!(prefixes.len(), 3);
+    assert!(prefixes.contains(&rulekey!(L1 58)));
+    assert!(prefixes.contains(&rulekey!(L1 29)));
+    assert!(prefixes.contains(&rulekey!(L1 29, R1 36)));
+}
