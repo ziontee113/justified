db10c6befff5b23fd86a81c2f05732475791f02e
ziontee113 <ziontee113@gmail.com>
2022-12-02 14:53:44 +0700

    feat: add rule! macro to create new Rule

--- src/rule_library/rule/mod.rs
+++ src/rule_library/rule/mod.rs
@@ -3,6 +3,13 @@ use crate::units::KeyIdentifier;
 #[cfg(test)]
 mod test;
 
+#[macro_export]
+macro_rules! rule {
+    ($($a:ident $b:expr), * => $output:expr) => {
+        $crate::rule_library::rule::Rule::new( vec![ $(ki!($a $b)),* ], $output )
+    };
+}
+
 pub struct Rule {
     input: Vec<KeyIdentifier>,
     output: u16,
--- src/rule_library/rule/test.rs
+++ src/rule_library/rule/test.rs
@@ -1,12 +1,8 @@
-use super::Rule;
-use crate::units::KeyIdentifier;
+use crate::{ki, rule};
 
 #[test]
-fn create_new_rule_with_multiple_input_and_single_output() {
-    let rule = Rule::new(
-        vec![KeyIdentifier::new("L1", 29), KeyIdentifier::new("R1", 36)],
-        115,
-    );
+fn can_create_new_rule_with_multiple_inputs_and_single_output_using_macro() {
+    let rule = rule!(L1 29, R1 36 => 115);
 
     assert_eq!(rule.input.get(0).unwrap().device_alias(), "L1");
     assert_eq!(rule.input.get(0).unwrap().code(), 29);
