4a848ce2a695283da7f104328f0ffe338c1d1660
ziontee113 <ziontee113@gmail.com>
2022-12-05 01:29:59 +0700

    rf(tests): gradually replace old tests in exec mod

--- src/executioner/test_new.rs
+++ src/executioner/test_new.rs
@@ -85,14 +85,145 @@ macro_rules! tsc {
     };
 }
 
+// Aliases:
+// 1. mono --> non-modifier key
+// 2. mono-combo --> combo is not modifier of another combo
+// 3. modifer --> modifier key
+// 4. modifer-combo --> modifier combo that is modifier of another combo
+
 #[test]
-fn non_prefix_key_down_and_up() {
+fn mono_key_down_and_up() {
     receive_and_assert(
         &mut State::new(),
         &mock_ruleset(),
         vec![
-            tsc!(L1 ESC, 1, 0 => Some(58), "wanted 58"),
-            tsc!(L1 ESC, 0, 100 => None, "didn't expect to see result"),
+            tsc!(L1 ESC, 1, 0 => Some(58), "esc down, mono, wanted 58"),
+            tsc!(L1 ESC, 0, 50 => None, "esc up, mono, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn consecutive_mono_key_down_and_up() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 ESC, 1, 0 => Some(58), "esc down, mono, wanted 58 (Capslock)"),
+            tsc!(L1 ESC, 0, 50 => None, "esc up, mono, expected none"),
+            tsc!(L1 INSERT, 1, 100 => Some(127), "insert down, mono, wanted 127 (Compose)"),
+            tsc!(L1 INSERT, 0, 150 => None, "insert up, mono, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn modifier_key_down_and_up() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 CAPSLOCK, 1, 0 => None, "Capslock down, modifer, expected none"),
+            tsc!(L1 CAPSLOCK, 0, 50 => Some(1), "Capslock up, modifier, wanted 1 (Escape)"),
+        ],
+    );
+}
+
+#[test]
+fn mono_combo() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 CAPSLOCK, 1, 0 => None, "Caps down, modifier, expected none"),
+            tsc!(R1 J, 1, 50 => Some(108), "J down, mono-combo, wanted 108 (Down)"),
+            tsc!(R1 J, 0, 100 => None, "J up, mono-combo, expected none"),
+            tsc!(L1 CAPSLOCK, 0, 150 => None, "Caps up, modifier, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn consecutive_mono_combos() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 CAPSLOCK, 1, 0 => None, "Caps down, modifier, expected none"),
+            tsc!(R1 J, 1, 50 => Some(108), "J down, mono-combo, wanted 108 (Down)"),
+            tsc!(R1 J, 0, 100 => None, "J up, mono-combo, expected none"),
+            tsc!(R1 K, 1, 150 => Some(103), "K down, mono-combo, wanted 103 (Up)"),
+            tsc!(R1 K, 0, 200 => None, "K up, mono-combo, expected none"),
+            tsc!(L1 CAPSLOCK, 0, 400 => None, "Caps up, modifier, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn chain_2_modifier_combo() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 LEFTCTRL, 1, 0 => None, "LeftCtrl down, modifier, expected none"),
+            tsc!(R1 J, 1, 50 => None, "J down, modifier-combo, expected none"),
+            tsc!(R1 J, 0, 100 => Some(116), "J down, modifier-combo, wanted 116 (VolumeDown)"),
+            tsc!(L1 LEFTCTRL, 0, 200 => None, "LeftCtrl up, modifier, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn consecutive_chain_2_modifier_combos() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 LEFTCTRL, 1, 0 => None, "LeftCtrl down, modifier, expected none"),
+            tsc!(R1 J, 1, 50 => None, "J down, modifier-combo, expected none"),
+            tsc!(R1 J, 0, 100 => Some(116), "J down, modifier-combo, wanted 116 (VolumeDown)"),
+            tsc!(R1 K, 1, 150 => None, "K down, modifier-combo, expected none"),
+            tsc!(R1 K, 0, 200 => Some(115), "K down, modifier-combo, wanted 115 (VolumeUp)"),
+            tsc!(L1 LEFTCTRL, 0, 300 => None, "LeftCtrl up, modifier, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn chain_3_modifier_combos() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 LEFTCTRL, 1, 0 => None, "LeftCtrl down, modifier, expected none"),
+            tsc!(R1 J, 1, 50 => None, "J down, modifier-combo, expected none"),
+            tsc!(R1 K, 1, 100 => Some(26), "K down, mono-combo, wanted 26 (RightBrace)"),
+            tsc!(R1 K, 0, 150 => None, "K up, mono-combo, expected none"),
+            tsc!(R1 J, 0, 200 => None, "J up, modifier-combo, expected none"),
+            tsc!(L1 LEFTCTRL, 0, 250 => None, "LeftCtrl up, modifier, expected none"),
+        ],
+    );
+}
+
+#[test]
+fn consecutive_chain_3_modifier_combos() {
+    receive_and_assert(
+        &mut State::new(),
+        &mock_ruleset(),
+        vec![
+            tsc!(L1 LEFTCTRL, 1, 0 => None, "LeftCtrl down, modifier, expected none"),
+            // first round start
+            tsc!(R1 J, 1, 50 => None, "C-J down, modifier-combo, expected none"),
+            tsc!(R1 K, 1, 100 => Some(26), "C-J-K down, mono-combo, wanted 26 (LeftBrace)"),
+            tsc!(R1 K, 0, 150 => None, "K up, mono-combo, expected none"),
+            tsc!(R1 J, 0, 200 => None, "J up, modifier-combo, expected none"),
+            // first round end, second round start
+            tsc!(R1 K, 1, 250 => None, "C-K down, modifier-combo, expected none"),
+            tsc!(R1 J, 1, 300 => Some(27), "C-K-J down, mono-combo, wanted 27 (RightBrace)"),
+            tsc!(R1 J, 0, 350 => None, "J up, modifier-combo, expected none"),
+            tsc!(R1 K, 0, 400 => None, "K up, mono-combo, expected none"),
+            // second round end
+            tsc!(L1 LEFTCTRL, 0, 500 => None, "LeftCtrl up, modifier, expected none"),
         ],
     );
 }
